13c589f40a42134c134fae74f4799fe34a1d1b55
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-07-18 05:22:55 -0700

    reorganise code and fix duplicate bug

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,20 +3,8 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+/* global $  document timeago alert*/
 $(document).ready(function () {
-  // const tweetData = {
-  //   user: {
-  //     name: 'Newton',
-  //     avatars: 'https://i.imgur.com/73hZDYK.png',
-  //     handle: '@SirIsaac'
-  //   },
-  //   content: {
-  //     text: 'If I have seen further it is by standing on the shoulders of giants'
-  //   },
-  //   created_at: 1461116232227
-  // };
-
   //function to create a new tweetstructure and display it
   const $createTweetElement = function (data) {
     for (const info in data) {
@@ -42,54 +30,16 @@ $(document).ready(function () {
     }
   };
 
-  // const $tweet = $createTweetElement(tweetData);
-
   //function to display all tweets from users on the page
   const $renderTweets = function (array) {
+    $('.tweets-container').empty();
     for (const item of array) {
       //calling createTweet on each tweet
       const $singleTweet = $createTweetElement(item);
       //appending each tweet to the tweet-container on the page
-      $('#tweets-container').append($singleTweet);
+      $('#tweets-container').prepend($singleTweet);
     }
   };
-  // $renderTweets(data);
-
-  //Posting a tweet using the form
-  const $form = $('form');
-  const $tweetInput = $('#tweet-text');
-
-  $form.submit(function (event) {
-    event.preventDefault();
-
-    const $tweetValue = $tweetInput.val();
-
-    $(this).serialize();
-
-    //form validation
-    //checking if the form is empty
-    if (!$tweetValue) {
-      alert('please input a message');
-      return;
-    }
-
-    //checking if the tweet isi longer than 140 characters
-    if ($tweetValue.length > 140) {
-      alert('maximum tweet length exceeded');
-      return;
-    }
-
-    //sbmittingthe tweet via Ajax
-    $.ajax({
-      url: '/tweets',
-      success: (response) => {
-        console.log(response);
-      },
-      error: (error) => {
-        console.log('error', error);
-      }
-    });
-  });
 
   //using Ajax to makea get request from '/tweets'
   const loadTweets = function () {
@@ -103,4 +53,38 @@ $(document).ready(function () {
     });
   };
   loadTweets();
+
+  //Posting a tweet using the form
+
+  const $form = $('form'); //getting the form from the dom
+  const $tweetInput = $('#tweet-text');
+
+  $form.submit(function (event) {
+    event.preventDefault();
+
+    const $tweetValue = $tweetInput.val();
+
+    //form validation
+    //checking if the form is empty
+    if (!$tweetValue) {
+      alert('please input a message');
+      return;
+    }
+    //checking if the tweet isi longer than 140 characters
+    if ($tweetValue.length > 140) {
+      alert('maximum tweet length exceeded');
+      return;
+    }
+
+    //posting the tweet to the page
+    $.ajax({
+      url: '/tweets',
+      type: 'POST',
+      data: $(this).serialize(),
+      success: (data) => {
+        console.log(data);
+        loadTweets();
+      }
+    });
+  });
 });
