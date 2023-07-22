903b73f9e52b5a83ecf37d4687ac9dc8912b87ce
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-07-22 14:04:06 -0700

    clean up code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,17 +3,17 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-/* global $  document timeago alert*/
-$(document).ready(function () {
+/* global $  document timeago */
+$(document).ready(function() {
   //function to escape insecure text and prevent cross-site scripting
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
   //function to create a new tweetstructure and display it
-  const $createTweetElement = function (data) {
+  const $createTweetElement = function(data) {
     for (const info in data) {
       return `<article class="tweet" >
 				<header class="tweet-header">
@@ -21,7 +21,7 @@ $(document).ready(function () {
 						<img src=${data.user.avatars} alt="profile-pic">
 						<p>${data.user.name}</p>
 					</div>
-					<p>${data.user.handle}</p>
+					<p class="user-handle">${data.user.handle}</p>
 				</header>
 				<p class="tweet-content">${escape(data.content.text)}</p>
 				<footer>
@@ -38,36 +38,32 @@ $(document).ready(function () {
   };
 
   //function to display all tweets from users on the page
-  const $renderTweets = function (array) {
-    $('.tweets-container').empty();
+  const $renderTweets = function(array) {
+    $('.tweets').empty();
     for (const item of array) {
       //calling createTweet on each tweet
       const $singleTweet = $createTweetElement(item);
-      //appending each tweet to the tweet-container on the page
-      $('#tweets-container').prepend($singleTweet);
+      //appending each tweet to the tweets on the page
+      $('#tweets').prepend($singleTweet);
     }
   };
   //creating the error messages
   const $error = $('<article>').addClass('error');
-  const $errMessage1 = $(`<p>🛑 Please enter a thought!!! 🛑</p>`);
-  const $errMessage2 = $(`<p>🛑 maximum tweet length exceeded 🛑</p>`);
+  const $errMessage1 = $(`<p>🛑 Please enter a thought!!! 🛑</p>`).addClass('errMessage1');
+  const $errMessage2 = $(`<p>🛑 maximum tweet length exceeded 🛑</p>`).addClass('errMessage12');
 
-  const $errorMessage = function (message) {
+  const $errorMessage = function(message) {
     $error.append(message);
-    $('.container').prepend($error);
-
-    // $error.slideDown(1000, function () {
-    //   console.log('finished');
-    // });
+    $('.tweets-container').prepend($error);
+    if ($('.error').is(':hidden')) {
+      $('.error').slideDown('slow', function() {});
+    }
   };
 
-  //function to remove error
-  const $removeErrorMessage = function () {
-    $('.error').empty();
-  };
+  
 
   //using Ajax to makea get request from '/tweets'
-  const loadTweets = function () {
+  const loadTweets = function() {
     $.ajax({
       url: 'http://localhost:8080/tweets',
       type: 'GET',
@@ -84,32 +80,44 @@ $(document).ready(function () {
   const $form = $('form'); //getting the form from the dom
   const $tweetInput = $('#tweet-text');
 
-  $form.submit(function (event) {
+  $form.submit(function(event) {
     event.preventDefault();
-    //remove error on submit of the form
-    $removeErrorMessage();
-
     const $tweetValue = $tweetInput.val();
+
+    /// Validations are checked when the user presses the  Submit button
 
     //form validation
     //checking if the form is empty
+
     if (!$tweetValue) {
       $errorMessage($errMessage1);
       return;
+    } else {
+      // $removeErrorMessage($errMessage1);
+      $('.error').hide();
     }
-    //checking if the tweet isi longer than 140 characters
+    //checking if the tweet is longer than 140 characters
     if ($tweetValue.length > 140) {
       $errorMessage($errMessage2);
       return;
+    } else {
+      $('.error').hide();
+      // $removeErrorMessage($errMessage2);
     }
 
+    const $data = $(this).serialize();
+    console.log($data);
     //posting the tweet to the page
     $.ajax({
       url: '/tweets',
       type: 'POST',
-      data: $(this).serialize(),
-      success: (data) => {
-        console.log(data);
+      data: $data,
+      success: () => {
+        // reset the form input
+        $form.trigger('reset');
+        //reset the counter
+        $('.counter').text(140);
+        //reloads the tweets from server to page
         loadTweets();
       }
     });
